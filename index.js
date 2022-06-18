4a666c5d1fd51b01a4446325c1f320ccaf0365ce
shilu kumari <silugilu13@gmail.com>
2022-06-18 22:33:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,1 @@
+document.getElementById("d1").innerHTML="i m changing this line"
---
+++ variable.js
@@ -0,0 +1,12 @@
+var a = 10;
+console.log(c);
+var b;  // variable declear
+console.log(b);
+b = 15;  // assign value
+
+
+console.log(b);
+console.log(a);
+
+
+
